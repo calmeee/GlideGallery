1e227ba5b3344856d7733b7d2fbb642441ae493b
Oleg N <baneful@mail.ru>
2017-04-19 02:25:13 +0300

    Initial commit

---
+++ app/src/main/java/com/android/glidegallery/MainActivity.java
@@ -0,0 +1,104 @@
+package com.android.glidegallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+public class MainActivity extends AppCompatActivity {
+
+    private class ImageGalleryAdapter extends RecyclerView.Adapter<ImageGalleryAdapter.MyViewHolder>  {
+
+        @Override
+        public ImageGalleryAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            Context context = parent.getContext();
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View photoView = inflater.inflate(R.layout.item_photo, parent, false);
+            ImageGalleryAdapter.MyViewHolder viewHolder = new ImageGalleryAdapter.MyViewHolder(photoView);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ImageGalleryAdapter.MyViewHolder holder, int position) {
+
+            SpacePhoto spacePhoto = mSpacePhotos[position];
+            final ImageView imageView = holder.mPhotoImageView;
+
+            Glide.with(mContext)
+                    .load(spacePhoto.getUrl())
+                    .asBitmap()
+                    .fitCenter()
+                    .centerCrop()
+                    .skipMemoryCache(true)
+                    .placeholder(R.drawable.ic_load)
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .into(imageView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return (mSpacePhotos.length);
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            public ImageView mPhotoImageView;
+
+            public MyViewHolder(View itemView) {
+
+                super(itemView);
+                mPhotoImageView = (ImageView) itemView.findViewById(R.id.iv_photo);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View view) {
+
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION) {
+                    SpacePhoto spacePhoto = mSpacePhotos[position];
+                    Intent intent = new Intent(mContext, SpacePhotoActivity.class);
+                    intent.putExtra(SpacePhotoActivity.EXTRA_SPACE_PHOTO, spacePhoto);
+                    startActivity(intent);
+                }
+            }
+        }
+
+        private SpacePhoto[] mSpacePhotos;
+        private Context mContext;
+
+        public ImageGalleryAdapter(Context context, SpacePhoto[] spacePhotos) {
+            mContext = context;
+            mSpacePhotos = spacePhotos;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_images);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ImageGalleryAdapter adapter = new ImageGalleryAdapter(this, SpacePhoto.getSpacePhotos());
+        recyclerView.setAdapter(adapter);
+    }
+}
